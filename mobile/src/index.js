dfdb4ce845aab5d9663b319a7002d8fbb13b491b
David Moreira <davidmoreira37@gmail.com>
2020-09-06 15:23:28 +0100

    Add creation of projectfrom mobile using api

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 
 import api from './services/api';
 
@@ -13,6 +13,17 @@ export default function App() {
     });
   }, []);
 
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Project ${Date.now()}`,
+      owner: 'owner'
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
@@ -25,6 +36,14 @@ export default function App() {
             <Text style={styles.project}>{project.title}</Text>
           )}
         />
+
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.6}
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Add project</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
@@ -39,5 +58,19 @@ const styles = StyleSheet.create({
   project: {
     color: '#FFF',
     fontSize: 32
+  },
+
+  button: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16
   }
 });
